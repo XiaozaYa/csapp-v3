169295280b7abf6569f34f6e9e5f050e9432033d
XiaozaYa <xiaozazhou@gmail.com>
2023-08-05 20:08:16 +0800

    8-5

---
+++ test/1.s
@@ -0,0 +1,21 @@
+	.file	"1.c"
+	.text
+	.globl	func
+	.type	func, @function
+func:
+.LFB0:
+	.cfi_startproc
+	cmpq	%rsi, %rdi
+	jge	.L2
+	movq	%rsi, %rax
+	subq	%rdi, %rax
+	ret
+.L2:
+	movq	%rdi, %rax
+	subq	%rsi, %rax
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	func, .-func
+	.ident	"GCC: (Ubuntu 5.5.0-12ubuntu1~16.04) 5.5.0 20171010"
+	.section	.note.GNU-stack,"",@progbits
